34bf78a633d736d7eca2962001fdcf555072c0f0
Andrey Zzaq <37836034+zzaq17@users.noreply.github.com>
2022-02-15 19:41:22 +0300

    metrika_to_model

--- metrika_to_model.php
+++ metrika_to_model.php
@@ -3,27 +3,22 @@ require_once 'settings/config.php';
 require_once 'settings/auth.php';
 require_once 'settings/gsheet-auth.php';
 
+// функция добавляет в таблицу данные
+function appendVisits() {
 
-function updateGsheet() {
-
-	global $counterIDs;
 	global $headers;
-	global $reports;
-	global $goalTypes;
-	global $goalCondsTypes;
 	global $service;
 
 	$spreadsheetId = '1dQgxqMHVwApxEp1ZqcODMlXgX3UmKzoBsh_RTuHt6Ss';
 
+	// Настройки запроса к Метрике
+	$date1 = '2020-01-01';
+	$date2 = '2020-12-31';
+	$today = date('yyyy-mm-dd');
+	
+	$fullArray = [];
 
-	$reportType = 'goals';
-		// Массив, в который добавятся все строки с целями
-		$fullArray = [];
-
-		// Цикл перебора счетчиков Метрики из config.php
-		foreach ($counterIDs as $key => $counterID) {
-
-			$request = 'https://api-metrika.yandex.net/management/v1/counter/'. $counterID .'/'.$reports[$reportType].'';
+			$request = "https://api-metrika.yandex.net/stat/v1/data/bytime?metrics=ym:s:visits,ym:s:users&ids=50212432,48002045,50337838,56942572,57655366,62131978,72684979,46733565,49221070,46696410,49052279,51933194&accuracy=1&date1=".$date1."&date2=".$date2."&dimensions=ym:s:startURLDomain&filters=ym:s:isRobot=='No'&group=month&include_annotations=false&proposed_accuracy=true&sort=-ym:s:users";
 			$curl = curl_init();
 			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -32,68 +27,73 @@ function updateGsheet() {
 			// curl_setopt($curl, CURLOPT_POST, true); // true - означает, что отправляется POST запрос
 			
 			$result = curl_exec($curl);
-			$goals = json_decode($result)->goals;
-			$currentDomain = $key;
+			$resultDecode = json_decode($result);
+			$data = $resultDecode->data;
+			$timeIntervals = $resultDecode->time_intervals;
 
-			foreach ($goals as $key => $val) {
-				$g = $goals[$key];
-				if (property_exists($g, 'conditions')) {
-					$c = $g->conditions[0];
-				}
+			var_dump($data);
+			var_dump($timeIntervals);
+
+			// $currentDomain = $key;
+
+			// foreach ($goals as $key => $val) {
+			// 	$g = $goals[$key];
+			// 	if (property_exists($g, 'conditions')) {
+			// 		$c = $g->conditions[0];
+			// 	}
 				
-					switch ($g->type) {
+			// 		switch ($g->type) {
 
-						case 'url':
-									$fullArray[] = [
-										$currentDomain,
-										$g->id,
-										$g->name,
-										$goalTypes[$g->type],
-										$g->is_retargeting,
-										$g->goal_source,
-										$g->default_price,
-										$goalCondsTypes[$c->type],
-										$c->url,
-									];
-						break;
+			// 			case 'url':
+			// 						$fullArray[] = [
+			// 							$currentDomain,
+			// 							$g->id,
+			// 							$g->name,
+			// 							$goalTypes[$g->type],
+			// 							$g->is_retargeting,
+			// 							$g->goal_source,
+			// 							$g->default_price,
+			// 							$goalCondsTypes[$c->type],
+			// 							$c->url,
+			// 						];
+			// 			break;
 
-						case 'action':
-									$fullArray[] = [
-										$currentDomain,
-										$g->id,
-										$g->name,
-										$goalTypes[$g->type],
-										$g->is_retargeting,
-										$g->goal_source,
-										$g->default_price,
-										$goalCondsTypes[$c->type],
-										$c->url,
-									];
-									break;
+			// 			case 'action':
+			// 						$fullArray[] = [
+			// 							$currentDomain,
+			// 							$g->id,
+			// 							$g->name,
+			// 							$goalTypes[$g->type],
+			// 							$g->is_retargeting,
+			// 							$g->goal_source,
+			// 							$g->default_price,
+			// 							$goalCondsTypes[$c->type],
+			// 							$c->url,
+			// 						];
+			// 						break;
 									
-									default:
-									$fullArray[] = [
-										$currentDomain,
-										$g->id,
-										$g->name,
-										$goalTypes[$g->type],
-										$g->is_retargeting,
-										$g->goal_source,
-										$g->default_price,
-									];
-							break;
-						}
-				}
-		}
+			// 						default:
+			// 						$fullArray[] = [
+			// 							$currentDomain,
+			// 							$g->id,
+			// 							$g->name,
+			// 							$goalTypes[$g->type],
+			// 							$g->is_retargeting,
+			// 							$g->goal_source,
+			// 							$g->default_price,
+			// 						];
+			// 				break;
+			// 			}
+			// 	}
 
 		// Отправка массива в Google Sheets
 		$ValueRange = new Google_Service_Sheets_ValueRange(['values' => $fullArray]);
 		// Указываем в опциях обрабатывать пользовательские данные
 		$options = ['valueInputOption' => 'RAW'];
 		// Имя листа для вставки
-		$list = 'Выгрузка всех визитов';
+		$list = "'Выгрузка всех визитов'!";
 		// Ячейка, с которой начинаем вставку
-		$row = $list . 'A2';
+		$row = $list . "A2";
 		
 			// Делаем запрос с указанием во втором параметре названия листа и начальную ячейку для заполнения
 			$result = $service->spreadsheets_values->update($spreadsheetId, $row, $ValueRange, $options);
@@ -101,4 +101,4 @@ function updateGsheet() {
 	return $result;
 }
 
-updateGsheet();
+appendVisits();
